b978342753d51a408391b969dc503cc9c919b58b
kassteele <jan.vande.kassteele@rivm.nl>
2024-01-18 10:45:16 +0100

    date_start and date_end to join data script

--- KorthalsAltes_COVID19_absenteeism/scripts/04_join_datasources.R
+++ KorthalsAltes_COVID19_absenteeism/scripts/04_join_datasources.R
@@ -2,12 +2,17 @@
 # Join data sources
 #
 
-# Join data_infectieradar to data_absent
+# Join data_infectionradar to data_absent
 data_absent <- left_join(
   x = data_absent,
-  y = data_infectieradar,
+  y = data_infectionradar,
   by = "Week") |>
   # Drop all records without perc_symptoms
   # These are the weeks where Infectieradar was not active yet
   drop_na(
     perc_symptoms)
+
+# Period: start date (Monday of 1st week) and end date (Sunday of last week)
+# Used for plotting
+date_start <- data_absent |> pull(Week) |> min() |> format("%b %-e, %Y")
+date_end <- data_absent |> pull(Week) |> max() |> (`+`)(days(6)) |> format("%b %-e, %Y")
